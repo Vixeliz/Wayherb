243084e2e5f89ffdb9783d89d6d45584f72d14d2
muevoid <38061852+muevoid@users.noreply.github.com>
2021-04-23 20:00:43 -0500

    Overlap

--- src/wayherb.c
+++ src/wayherb.c
@@ -86,8 +86,8 @@ int main(int argc, char *argv[])
 	sigaction(SIGUSR1, &act_ignore, 0);
 	sigaction(SIGUSR2, &act_ignore, 0);
 	
-	sem_t *mutex = sem_open("/wayherb", O_CREAT, 0644, 1);
-	sem_wait(mutex);
+	//sem_t *mutex = sem_open("/wayherb", O_CREAT, 0644, 1);
+	//sem_wait(mutex);
 	
 	sigaction(SIGUSR1, &act_expire, 0);
 	sigaction(SIGUSR2, &act_expire, 0);
@@ -111,8 +111,8 @@ int main(int argc, char *argv[])
 
         init_wayland(argc, argv);
 
-	if (duration != 0)
-		alarm(duration);
+	//if (duration != 0)
+		//alarm(duration);
 	
 	
 	for (;;) {
@@ -131,8 +131,8 @@ int main(int argc, char *argv[])
 		break;
 	}
 
-	sem_post(mutex);
-	sem_close(mutex);
+	//sem_post(mutex);
+	//sem_close(mutex);
 	
 	free_wayland();
 	return exit_code;
